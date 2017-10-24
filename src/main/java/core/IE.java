f7b68ff913e206c38c9c6f3a8ffaf2c483bf5be3
Bakhtiyor Sony <bakhtiyor.makhsimov@am.sony.com>
2017-10-24 11:37:02 -0700

    updated IE driver and fixed IE.java

--- src/main/java/core/IE.java
+++ src/main/java/core/IE.java
@@ -4,68 +4,70 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.util.concurrent.TimeUnit;
 import java.util.logging.*;
 
 public class IE {
-	
-	static WebDriver driver;
 
-	public static void main(String[] args) throws InterruptedException {
+       static WebDriver driver;
+       public static void main(String[] args) throws InterruptedException {
 
-		Logger logger = Logger.getLogger("");
-		logger.setLevel(Level.OFF);
+              Logger logger = Logger.getLogger("");
+              logger.setLevel(Level.OFF);
 
-		String driverPath = "./resources/webdrivers/pc/IEDriverServer.exe";
+              String driverPath = "./resources/webdrivers/pc/IEDriverServer.exe";
 
-		String url = "http://facebook.com/";
-		String email_address = "abc@gmail.cc";
-		String password = "*******************";
-		
-		if (!System.getProperty("os.name").contains("Windows")) {
-			throw new IllegalArgumentException("Internet Explorer is available only on Windows");
-		}
+              String url = "http://facebook.com/";
+              String email_address = "publicbmuser@smsstam.net";
+              String password = "Buska@2017";
 
-		DesiredCapabilities IEDesiredCapabilities = DesiredCapabilities.internetExplorer();
-		IEDesiredCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-		IEDesiredCapabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "");
-		IEDesiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-		IEDesiredCapabilities.setJavascriptEnabled(true);
-		IEDesiredCapabilities.setCapability("enablePersistentHover", false);
+              if (!System.getProperty("os.name").contains("Windows")) {
+                     throw new IllegalArgumentException("Internet Explorer is available only on Windows");
+              }
 
-		System.setProperty("webdriver.ie.driver", driverPath);
-		driver = new InternetExplorerDriver(IEDesiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+              DesiredCapabilities IEDesiredCapabilities = DesiredCapabilities.internetExplorer();
+              IEDesiredCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+              IEDesiredCapabilities.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "");
+              IEDesiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+              IEDesiredCapabilities.setJavascriptEnabled(true);
+              IEDesiredCapabilities.setCapability("enablePersistentHover", false);
 
-		driver.get(url);
+              System.setProperty("webdriver.ie.driver", driverPath);
+              driver = new InternetExplorerDriver(IEDesiredCapabilities);
+              driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+              driver.manage().window().maximize();
 
-		Thread.sleep(1000); // Pause in milleseconds (1000 – 1 sec)
+              driver.get(url);
 
-		String title = driver.getTitle();
-		String copyright = driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getText();
+              Thread.sleep(1000); // Pause in milleseconds (1000 – 1 sec)
 
-		driver.findElement(By.id("email")).sendKeys(email_address);
-		driver.findElement(By.id("pass")).sendKeys(password);
-		driver.findElement(By.id("u_0_2")).click();
+      		  String title = driver.getTitle();
+    		  String copyright = driver.findElement(By.xpath("//*[@id=\'pageFooter\']/div[3]/div/span")).getText();
+    		
+    		  driver.findElement(By.id("email")).clear();
+    		  driver.findElement(By.id("email")).sendKeys(email_address);
+    		  driver.findElement(By.id("pass")).sendKeys(password);
+              driver.findElement(By.id("loginbutton")).click();
+            
+              Thread.sleep(1000);
+              driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).click();
 
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//*[@id='u_0_a']/div[1]/div[1]/div/a/span")).click();
+              Thread.sleep(1000);
+              String friends = driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getText();
+            
+              if (friends.length() == 0){
+            	friends = "NO";}
+                    	        
+              Thread.sleep(1000);
+              driver.findElement(By.id("userNavigationLabel")).click();
+              driver.findElement(By.xpath("//span[text()='Log Out']")).click();
+            
+              Thread.sleep(1000);
+              driver.quit();
 
-		Thread.sleep(1000);
-		String friends = driver.findElement(By.xpath("//div[2]/ul/li[3]/a/span[1]")).getText();
-
-		Thread.sleep(1000);
-		driver.findElement(By.id("userNavigationLabel")).click();
-		driver.findElement(By.xpath("//li[22]/a/span/span")).click();
-
-		Thread.sleep(1000);
-		driver.quit();
-
-		System.out.println("Browser is: Firefox");
-		System.out.println("Title of the page: " + title);
-		System.out.println("Copyright: " + copyright);
-		System.out.println("You have " + friends + " friends");
-	}
+              System.out.println("Browser is: IE");
+              System.out.println("Title of the page: " + title);
+              System.out.println("Copyright: " + copyright);
+              System.out.println("You have " + friends + " friends");
+       }
 }
